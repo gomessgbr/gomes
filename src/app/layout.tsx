1e68e0f7dba4d93edc1b4a2fa94e05fb9a0ae91b
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-23 17:10:47 -0300

    lint: config and format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils"
-
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Gabriel Gomes",
@@ -17,14 +16,19 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
+  // eslint-disable-next-line no-undef
   children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-br">
-      <body className={cn(
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>{children}</body>
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
