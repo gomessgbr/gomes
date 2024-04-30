c6dab3d869ca6c2ef37d0a75f0e43f277fa2195f
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-30 14:58:00 -0300

    feat: refactor project session

--- src/components/projects-session/projects-session.tsx
+++ src/components/projects-session/projects-session.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-
-import projectCoffee from "../../assets/projects/image.png";
+"use client";
+import { Cards } from "../ui/cards/cards";
+import { personalProjects } from "@/utils/projects";
 
 export function ProjectsSession() {
   return (
-    <div className="grid place-items-center">
+    <div className="grid place-items-center bg-blue-200 dark:bg-blue-600 py-16">
       <h2 className="text-5xl text"> Projetos</h2>
-      <div>
-        <Image alt="" src={projectCoffee} width={400} height={400} />
-        <span>Coffee Delivery</span>
-        <p>E-Commerce de venda de café</p>
+      <div className="grid grid-rows-4 md:grid-flow-col gap-8 py-12 px-8 ">
+        {personalProjects.map((project) => (
+          <Cards key={project.id} {...project} />
+        ))}
       </div>
     </div>
   );
